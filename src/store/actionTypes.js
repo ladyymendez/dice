05148c5a8d936808c658fb3f417c321a72816e45
ladyymendez <ladyy.mendez@gmail.com>
2021-05-08 19:33:41 -0500

    Updates

--- src/store/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const ADD_USER = "ADD_USER";
-export const ADD_DICE = "ADD_DICE";
-export const ROLL_DICE = "ROLL_DICE";
-export const LAST_VALUES = "LAST_VALUES";
-export const NEXT_PLAYER = "NEXT_PLAYER";
-export const RESET_PLAYER = "RESET_PLAYER";
-export const TOTAL_PLAYER = "TOTAL_PLAYER";
